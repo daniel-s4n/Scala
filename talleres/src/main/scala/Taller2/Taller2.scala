a60c9a04f6ddd9ea6cd117faf6ae831582aee0bb
Daniel Caita <danielcaita@seven4n.com>
2021-02-25 22:48:28 -0500

    Se añadieron los primeros 6 puntos del taller 2

--- talleres/src/main/scala/Taller2/Taller2.scala
+++ talleres/src/main/scala/Taller2/Taller2.scala
@@ -4,7 +4,7 @@ import scala.annotation.tailrec
 
 sealed trait List[+A]
 case object Nil extends List[Nothing]
-case class Const [+A](h: A, t: List[A]) extends List[A]
+case class Const[+A](h: A, t: List[A]) extends List[A]
 
 object Taller2 extends App {
   // Notas de clase
@@ -26,4 +26,37 @@ object Taller2 extends App {
     if (as.isEmpty) Nil
     else Const(as.head, apply(as.tail: _*))
   }
+
+  // Ejercicio 2
+  def tail[A](ls: List[A]):List[A] = ls match{
+    case Nil => Nil
+    case Const(h, t) => t
+  }
+
+  // Ejercicio 3
+  def head[A](ls: List[A]):A = ls match{
+    case Nil => null
+    case Const(h, t) => h
+  }
+
+  // Ejercicio 4
+  def and(lst: List[Boolean]): Boolean = lst match{
+    case Nil => false
+    case Const(h, t) => h && and(t)
+  }
+
+  // Ejercicio 5
+  def or (lst: List[Boolean]): Boolean  = lst match{
+    case Nil => false
+    case Const(h, t) => h || or(t)
+  }
+
+  // Ejercicio 6
+  def max(lst: List[Int]): Int = lst match{
+    case Nil => 0
+    case Const(h, Nil) => h
+
+  }
+
+
 }
