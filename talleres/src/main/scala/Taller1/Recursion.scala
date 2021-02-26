f69f7cad4b9e98a2bb3548d4851ead49a0469166
Daniel Caita <danielcaita@seven4n.com>
2021-02-26 07:58:00 -0500

    Se añadieron los puntos faltantes del taller y solo queda pendiente las pruebas unitarias

--- talleres/src/main/scala/Taller1/Recursion.scala
+++ talleres/src/main/scala/Taller1/Recursion.scala
@@ -1,5 +1,21 @@
 package Taller1
+import scala.annotation.tailrec
 
 class Recursion {
+  //Ejercicio 16
+  def fibonacci(n:Int): Int = {
+    if (n == 0) 0
+    else if (n == 1) 1
+    else fibonacci(n - 1) + fibonacci(n - 2)
+  }
 
+  // Ejercicio 15-17
+  def factorial(n:Int): Int = {
+    @tailrec
+    def tail(n:Int, res:Int):Int = {
+      if (n == 1) res
+      else tail(n - 1, res * n)
+    }
+    tail(n, 1)
+  }
 }
--- talleres/src/main/scala/Taller1/Taller1.scala
+++ talleres/src/main/scala/Taller1/Taller1.scala
@@ -1,36 +1,52 @@
 package Taller1
 
 object Taller1 extends App{
+  // Ejercicio 1
   val areaTrianguloRectangulo = (a:Int) => (a*a) / 2
+
+  // Ejercicio 2
   val areaCirculo = new Function1 [Double, Double] {
     def apply(r:Double) = math.Pi * (r*r)
   }
 
+  //Ejercicio 3
   val calSalario = (devengado:Double, deducciones:Double) => devengado - deducciones
+  //Ejercicio 4
   val calSalarioBono = (devengado:Double, deducciones:Double) => devengado * 1.10 - deducciones
 
+  //Ejercicio 5
   def compSalario( salario:(Double, Double) => Double, devengado:Double, deducciones:Double ) {
     salario(devengado, deducciones)
   }
 
+  // Ejercicio 6
   def genCalSalarioBono(bono:Double): (Double, Double)=>Double = {
     (devengado: Double, deduccion: Double) => devengado * bono - deduccion
   }
 
+  // Ejercicio 7
   def calSalario5(devengado:Double, deduccion:Double):Double = {
     val salario: (Double, Double) => Double = genCalSalarioBono(1.05)
     salario(devengado, deduccion)
   }
 
+  // Ejercicio 8
   def calSalario20(devengado:Double, deduccion:Double):Double = {
     val salario: (Double, Double) => Double = genCalSalarioBono(1.2)
     salario(devengado, deduccion)
   }
 
+  // Ejercicio 9
   val bono = 1
   def calSalarioBonoClausura(devengados:Double, deducciones:Double): Double = {
     devengados * bono - deducciones
   }
+
+  //Ejercicio 11
+  val calCalario15 = genCalSalarioBono(0.15)
+
+  //Ejercicio 12
+  val calCalario100 = genCalSalarioBono(1.00)
 
   val triangulo = areaTrianguloRectangulo(2)
   val circulo = areaCirculo(3.3)
